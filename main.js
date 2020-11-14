a3a86d936cca155e375d9fbe8cbe30cf9b0e7d1e
romocesar <cesarromocontreras@gmail.com>
2020-11-13 19:55:40 -0700

    Added Numbers and Strings implementation of JS language

---
+++ main.js
@@ -0,0 +1,41 @@
+//Numbers in Javascript
+document.write("Numbers <br/>");
+document.write("MAX Number: " + Number.MAX_VALUE + "<br/>");
+document.write("MIN Number: " + Number.MIN_VALUE + "<br/>");
+
+//Strings in Javascript
+var esc_char_double_quote = "\"";
+var esc_char_single_quote = "\'";
+var esc_char_tab = "Tab right next to me \t Tab left from me";
+var esc_char_backslash = "\\";
+
+document.write("<br/> Strings <br/>");
+document.write("Escape Characters <br/>");
+document.write("Double quote: " + esc_char_double_quote + "<br/>");
+document.write("Single quote: " + esc_char_single_quote + "<br/>");
+document.write("Tab space *doesn't always work*: " + esc_char_tab + "<br/>");
+document.write("Backslash: " + esc_char_backslash + "<br/>");
+
+var first_name = "Cesar";
+var last_name = "Romo";
+var full_name = first_name + " " + last_name;
+
+document.write("My Full Name: " + full_name);
+
+//LEFT HERE in min 5:15
+
+//Booleans in Javascript
+
+//Symbols in Javascript (JS standard ES2015)
+
+//Objects in Javascript
+//Function
+//Array
+//Date
+//RegExp
+
+//null and undefined in Javascript
+
+//let
+//const
+//var
